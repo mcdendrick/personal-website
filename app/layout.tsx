3cdf049254b2148b88f4ecf8b1ae5503aa92b9c6
mcdendrick <115506627+mcdendrick@users.noreply.github.com>
2025-01-14 20:13:37 -0800

    add twitter-image and code using built-in OG image generation to create a dynamic approach of preview image for twitter as well as some fixes to the metadata layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,19 +24,16 @@ export const metadata: Metadata = {
     siteName: "Taylor McKendrick",
     locale: "en_US",
     type: "website",
-    images: [
-      {
-        url: "/intro-website.png",
-        width: 1200,
-        height: 630,
-        alt: "Taylor McKendrick - Full Stack Developer",
-      },
-    ]
   },
   twitter: {
     card: "summary_large_image",
     title: "Taylor McKendrick",
     description: "Full Stack Developer | Cancer Survivor | Problem Solver",
+    images: 
+      {
+        url: "/twitter-image",
+        alt: "Taylor McKendrick - Full Stack Developer",
+      },
   },
   icons: {
     icon: "/favicon.ico",
